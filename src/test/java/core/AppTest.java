0809f6a700699d1f7c1d05cc3e4f9bb7fb79437d
Olga Romero <oromero.git@gmail.com>
2014-05-24 18:34:44 -0700

    initial commit with report

--- src/test/java/core/AppTest.java
+++ src/test/java/core/AppTest.java
@@ -1,5 +1,5 @@
 package core;
-
+ 
 import static org.junit.Assert.*;
 
 import org.junit.After;
@@ -9,72 +9,47 @@ import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 
-@SuppressWarnings("unused")
+
 public class AppTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		System.out.println("BeforeClass method will be executed before first test method starts");
-	}
+@BeforeClass
+public static void BeforeClass() throws Exception {}
+@AfterClass
+public static void AfterClass() throws Exception {}
+@Before
+public void Before() throws Exception {}
+@After
+public void After() throws Exception {}
+      
+@Test
+public void Test_01_AssertEquals_Positive() {assertEquals("String not the same", "Testing JUnit 4", App.s);}
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-		System.out.println("AfterClass method will be executed after last test method completed");
-	}
+@Test
+public void Test_02_AssertEquals_Negative() {assertEquals("String not the same.", "Testing TestNG 6", App.s);}
 
-	@Before
-	public void setUp() throws Exception {
-		System.out.println("Before method will execute before every test method");
-	}
+@Ignore
+@Test
+public void Test_03_AssertEquals_Ignored() {assertEquals("String not the same.", "Testing JUnit 3", App.s);}
 
-	@After
-	public void tearDown() throws Exception {
-		System.out.println("After method will execute after every test method");
-	}
+@Test
+public void Test_04_AssertSame_Positive() {assertSame("Integer not the same.", 55,App.i);}
 
-	@Test
-	public void Test_01_assertEquals_Positive() {
-		System.out.println("Test_01_assertEquals_Positive");
-	}
-	@Test
-	public void Test_02_assertEquals_Negative() {
-		System.out.println("Test_02_assertEquals_Negative");
-	}
-	@Ignore
-	@Test
-	public void Test_03_assertEquals_Ignored() {
-		System.out.println("Test_03_assertEquals_Ignored");
-	}
-	@Test
-	public void Test_04_AssertSame_Positive() {
-		System.out.println("Test_04_AssertSame_Positive");
-	}
-	@Test
-	public void Test_05_AssertSame_Negative() {
-		System.out.println("Test_05_AssertSame_Negative");
-	}
-	@Ignore
-	@Test
-	public void Test_06_AssertSame_Ignored() {
-	       System.out.println("Test_06_AssertSame_Ignored");
-	}
-	@Test
-	public void Test_07_assertFalse_Positive() {
-		System.out.println("Test_07_assertFalse_Positive");
-	}
-	@Test
-	public void Test_08_assertFalse_Negative() {
-		System.out.println("Test_08_assertFalse_Negative");
-	}
-	@Test
-	public void Test_09_assertTrue_Positive() {
-		System.out.println("Test_09_assertTrue_Positive");
-	}
-	@Test
-	public void Test_10_assertTrue_Negative() {
-		System.out.println("Test_10_assertTrue_Negative");
-	}
+@Test
+public void Test_05_AssertSame_Negative() {assertSame("Integer not the same.", 54,App.i);}
+
+@Ignore
+@Test
+public void Test_06_AssertSame_Ignored() {assertSame("Integer not the same.", 56,App.i);}
+
+@Test
+public void Test_07_AssertFalse_Positive() {assertFalse("Boolean should be false", App.f);}
+
+@Test
+public void Test_08_AssertFalse_Negative() {assertFalse("Boolean should be false", App.t);}
+      
+@Test
+public void Test_09_AssertTrue_Positive() {assertTrue("Boolean should be true", App.t);}
+      
+@Test
+public void Test_10_AssertTrue_Negative() {assertTrue("Boolean should be true", App.f);}
 }
-	
-	
-	
